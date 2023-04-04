53f5607f50b5d63806f377f9fa819bd2f06a1551
Yerkebulan <128957554+yerkebulan0@users.noreply.github.com>
2023-04-05 00:58:52 +0600

    web04

---
+++ web04.js
@@ -0,0 +1,89 @@
+
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       return confirm('Родители разрешили?');
+//     }
+//   }
+//   let age = prompt('Сколько вам лет?');
+//   checkAge();
+
+//   function checkAge(age) {
+//     return (age > 18) ? true : confirm('Родители разрешили?');
+//   }
+
+//   function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили?');
+//   }
+
+
+
+
+//   function pow(x, n) {
+//     let result = x;
+  
+//     for (let i = 1; i < n; i++) {
+//       result *= x;
+//     }
+  
+//     return result;
+//   }
+  
+//   let x = prompt("x?", '');
+//   let n = prompt("n?", '');
+  
+//   if (n < 1) {
+//     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+//   } else {
+//     alert( pow(x, n) );
+//   }
+
+
+//   function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   ask(
+//     "Вы согласны?",
+//     () => alert("Вы согласились."),
+//     () => alert("Вы отменили выполнение.")
+//   );
+
+//   let arr = [5, 2, 1, -10, 8];
+
+// arr.sort((a, b) => b - a);
+
+// alert( arr );
+
+
+
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map(user => ({
+//   fullName: `${user.name} ${user.surname}`,
+//   id: user.id
+// }));
+
+// alert( usersMapped[0].id ); // 1
+// alert( usersMapped[0].fullName );
+
+
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+  }
+  
+  let vasya = { name: "Вася", age: 25 };
+  let petya = { name: "Петя", age: 30 };
+  let masha = { name: "Маша", age: 29 };
+  
+  let arr = [ vasya, petya, masha ];
+  
+  alert( getAverageAge(arr) );
